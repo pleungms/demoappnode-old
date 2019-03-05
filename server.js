dc9f6dfd6ad2d5c9f4b0050d9e6c8b8c475e6ea3
Peter Leung <pleungms@hotmail.com>
2019-03-05 17:44:42 +0800

    removed config

--- server.js
+++ server.js
@@ -7,23 +7,24 @@
 //  Author         Date       Description
 //  -------------- ---------- -------------------------------------------------
 //  Peter Leung    13/09/2018 Initial version
+//  Peter Leung    05/03/2019 Removed config
 //
 // ############################################################################
 var express = require('express');
 var app = express();
 
 // Obtain the environment the application will run in
-var env = process.env.NODE_ENV || 'dev';
-var config = require('./config')[env];
+//var env = process.env.NODE_ENV || 'dev';
+//var config = require('./config')[env];
 
 // All routes
 app.get('/', function (req, res) {
   console.log('http get...');
-  res.send('Hello World! [ appname: ' + config.application.systeamprj + ' | buildid v' + config.application.buildid + ' ]');
+  res.send('Hello World!');
   //res.send('Hello World!');
 });
 
 // Create the server listening for http requests
-app.listen(config.server.port, function () {
+app.listen(80, function () {
   console.log('Example app listening on port ' + this.address().port);
 });
